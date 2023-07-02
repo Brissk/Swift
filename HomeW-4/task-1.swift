1ebbc994e44e325cf05a05f2ae0437da036ea82e
«Mikle <cobra-mikle@mail.ru>
2023-07-02 14:56:45 +0300

    hw4

--- HomeW-4/task-1.swift
+++ HomeW-4/task-1.swift
@@ -1,21 +1,51 @@
 /* 
-Задание 1:
- Создать класс Person, внутри которого должна быть структура Activity, 
-внутри которой должно быть перечисление TypeOfActivity (тип активности) 
-с тремя кейсами: pupil, student, employee. 
-Создать переменную и явно указать ей тип “тип активности”.
+Задания:
+1. Создать перечисление с видами пиццы (хотя бы 4-5 кейсов)
+2. Создать структуру пиццы. Она должна содержать стоимость, вид пиццы, толстое или тонкое тесто и добавки (например, дополнительно добавить пепперони, помидоры, сыр). Вид пиццы должен быть вложенным типом для структуры пиццы.
+3. Создать класс пиццерии, добавить массив с возможными видами пиццы. Переменная с массивом должна быть приватной. Массив задаётся в инициализаторе.
+4. Написать в классе пиццерии функции для добавления нового вида пиццы и для получения всех доступных пицц.
+5. Создать экземпляр класса пиццерии и добавить в него несколько видов пицц.
 */
 
-class Person {
+struct Pizza {
+    var cost:Int
+    var type: TypeOfPizza
 
-struct Activity {
+    enum Thickness {
+        case thick
+        case thin
+    }
     
-    enum TypeOfActivity {
-        case pupil
-        case student
-        case employee
+    enum Additive {
+        case pepperoni
+        case tomato
+        case cheeze
+    }
+    
+    enum TypeOfPizza {
+        case mozarella = "Mozarella"
+        case ceasar = "Ceasar"
+        case diablo = "Diablo"
+        case fourCheeze = "Four Cheeze"
     }
 }
+
+class Pizzeria {
+    private var pizzas:[Pizza] = []
+
+    init(pizzas:[Pizza]){
+      self.pizzas = pizzas
+   } 
+
+   func addPizza(pizza:Pizza.type)  {
+    self.pizzas.append(pizza)
+   }
+
+   func getAllPizzas() {
+    print(self.pizzas)    
+   }
 }
 
-var activ:Activity
+var myPizzeria = Pizzeria()
+myPizzeria.addPizza(.mozarella)
+myPizzeria.addPizza(.diablo)
